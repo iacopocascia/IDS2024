971943af780b93913d7ca1feac6347f988bd4c20
iacopocascia <iacopo.cascia@studenti.unicam.it>
2024-03-21 18:28:22 +0100

    Struttura iniziale progetto Springboot

---
+++ LocalDevelopmentPlatform/src/main/java/it/unicam/cs/ids/project2024/LocalDevelopmentPlatform/LocalDevelopmentPlatformApplication.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.project2024.LocalDevelopmentPlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalDevelopmentPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalDevelopmentPlatformApplication.class, args);
+	}
+
+}
