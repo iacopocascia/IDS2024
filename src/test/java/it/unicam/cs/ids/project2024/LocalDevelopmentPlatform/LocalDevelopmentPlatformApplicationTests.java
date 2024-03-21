fd0bc8643f371382844a26235fa8959622430de1
iacopocascia <iacopo.cascia@studenti.unicam.it>
2024-03-21 18:48:29 +0100

    Struttura progetto Springboot

---
+++ src/test/java/it/unicam/cs/ids/project2024/LocalDevelopmentPlatform/LocalDevelopmentPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.cs.ids.project2024.LocalDevelopmentPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalDevelopmentPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
